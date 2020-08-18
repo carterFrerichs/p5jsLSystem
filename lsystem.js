97bf3106483ebe4801485be5043f723d4aa1caf7
Carter Frerichs <carter@carters-MBP.attlocal.net>
2020-08-18 14:05:16 -0500

    format

--- lsystem.js
+++ lsystem.js
@@ -1,146 +1,145 @@
-class Lsystem{
-  
-  constructor(axiom = "F", rules = []){
-    
+class Lsystem {
+
+  constructor(axiom = "F", rules = []) {
+
     this.rules = rules;
-    if(this.rules.length <= 0){
+    if (this.rules.length <= 0) {
       this.rules[0] = {
         a: "F",
         b: "FF+[+F-F-F]-[-F+F+F]"
       };
     }
-    
+
     this.axiom = axiom;
     this.sentence = this.axiom;
-    this.lenMaster=100;
-    this.len=this.lenMaster;
+    this.lenMaster = 100;
+    this.len = this.lenMaster;
     this.angle = radians(25);
-     
+
     this.tree = [];
   }
-  
-  setAxiom(axiom){
+
+  setAxiom(axiom) {
     this.axiom = axiom;
   }
-  setLen(len){
-    this.lenMaster=len;
+  setLen(len) {
+    this.lenMaster = len;
   }
-  setAngle(angle){
-    this.angle=angle;
+  setAngle(angle) {
+    this.angle = angle;
   }
-  setRules(rules){
+  setRules(rules) {
     this.rules[0] = {
-                      a: "F",
-                      b: rules
+      a: "F",
+      b: rules
     };
   }
-  
-  generate(){
-    if(this.sentence.length > 2048)
-    {
+
+  generate() {
+    if (this.sentence.length > 2048) {
       this.sentence = this.axiom;
       this.len = this.lenMaster;
     }
-    
-    this.len*=0.5;
+
+    this.len *= 0.5;
     this.nextSentence = "";
-    
-    for(let i = 0; i < this.sentence.length; i++){
-    this.found = false;
+
+    for (let i = 0; i < this.sentence.length; i++) {
+      this.found = false;
       this.current = this.sentence.charAt(i);
-      
-      for(let j =0; j < this.rules.length; j++){
-        if(this.current == this.rules[j].a){
-           this.found = true;
-           this.nextSentence+=this.rules[j].b;
-           break;
-           } 
+
+      for (let j = 0; j < this.rules.length; j++) {
+        if (this.current == this.rules[j].a) {
+          this.found = true;
+          this.nextSentence += this.rules[j].b;
+          break;
+        }
 
 
       }
-      if(!this.found){
-        this.nextSentence+=this.current;
+      if (!this.found) {
+        this.nextSentence += this.current;
 
-       }
+      }
     }
 
-    this.sentence=this.nextSentence;
+    this.sentence = this.nextSentence;
 
 
   }
 
-  turtle(){
+  turtle() {
     resetMatrix();
-    translate(width/2, height);
+    translate(width / 2, height);
 
-    for(let i = 0 ; i < this.sentence.length;i++){
+    for (let i = 0; i < this.sentence.length; i++) {
       this.current = this.sentence.charAt(i);
-      if(this.current=="F"){
-           line(0, 0, 0, -this.len); 
-          
-           translate(0, -this.len);
-        
-         // this.tree.push([0, 0, 0, -this.len]);
-        
-      }else if (this.current=="+"){
-      rotate(this.angle);
-      }else if(this.current =="-"){
-      rotate(-this.angle);
+      if (this.current == "F") {
+        line(0, 0, 0, -this.len);
+
+        translate(0, -this.len);
+
+        // this.tree.push([0, 0, 0, -this.len]);
+
+      } else if (this.current == "+") {
+        rotate(this.angle);
+      } else if (this.current == "-") {
+        rotate(-this.angle);
       }
-      else if(this.current=="["){
-      push();
+      else if (this.current == "[") {
+        push();
       }
-      else if(this.current=="]"){
-      pop();
+      else if (this.current == "]") {
+        pop();
       }
     }
-      
+
     stroke(51, 100);
     //resetMatrix();
-    
-    for(let i of this.tree){
-  
-        let [beginX, endX, beginY, endY] = i;
-        line(beginX, endX, beginY, endY); 
-        translate(endX, endY);
-        this.len*=0.67;
 
-        // console.log(this.tree);
-    
+    for (let i of this.tree) {
+
+      let [beginX, endX, beginY, endY] = i;
+      line(beginX, endX, beginY, endY);
+      translate(endX, endY);
+      this.len *= 0.67;
+
+      // console.log(this.tree);
+
     }
     // console.log(this.tree);
   }
-  
-//   this.branch = {
-//   constructor(begin, end){
-//     this.begin = begin;
-//     this.end = end;
-//     this.finished=false;
-//   }
-  
-//   show(){
-//     stroke(51);
-//     line(this.begin.x, this.begin.y, 
-//          this.end.x, this.end.y);
-//   }
-  
-//   branch(){
-//     let direction = p5.Vector.sub(this.end, this.begin);
-    
-//     direction.rotate(PI/6);
-//     direction.mult(0.67);
-//     let rightEnd = p5.Vector.add(this.end, direction);
-//     direction.rotate(-PI/3);
-//     let leftEnd = p5.Vector.add(this.end, direction);
-//     let right = new Branch(this.end, rightEnd);
-//     let left = new Branch(this.end, leftEnd); 
-    
-//     return [right, left];
-//   }
+
+  //   this.branch = {
+  //   constructor(begin, end){
+  //     this.begin = begin;
+  //     this.end = end;
+  //     this.finished=false;
+  //   }
+
+  //   show(){
+  //     stroke(51);
+  //     line(this.begin.x, this.begin.y, 
+  //          this.end.x, this.end.y);
+  //   }
+
+  //   branch(){
+  //     let direction = p5.Vector.sub(this.end, this.begin);
+
+  //     direction.rotate(PI/6);
+  //     direction.mult(0.67);
+  //     let rightEnd = p5.Vector.add(this.end, direction);
+  //     direction.rotate(-PI/3);
+  //     let leftEnd = p5.Vector.add(this.end, direction);
+  //     let right = new Branch(this.end, rightEnd);
+  //     let left = new Branch(this.end, leftEnd); 
+
+  //     return [right, left];
+  //   }
 
 
 
-// }
+  // }
 
 
 
--- sketch.js
+++ sketch.js
@@ -4,72 +4,72 @@ let gui;
 let thetaSlider;
 // let lenSlider;
 // let radio;
-let rules = [['F','F','-','[','-','F','+','F','+','F',']','+','[','+','F','-','F','-','F',']'],['F','[','+','F',']','F','[','-','F',']','[','F',']'],['F','[','+','F',']','F','[','-','F',']','F'],['F','[','F','+','F','+','F',']','F','[','F','-','F','-','F',']'],['F','F','[','+','F',']','[','-','F',']','F','F','[','-','F','+','F','+','F',']'],['F','-','F','+','F','-','F','-','F']];
+let rules = [['F', 'F', '-', '[', '-', 'F', '+', 'F', '+', 'F', ']', '+', '[', '+', 'F', '-', 'F', '-', 'F', ']'], ['F', '[', '+', 'F', ']', 'F', '[', '-', 'F', ']', '[', 'F', ']'], ['F', '[', '+', 'F', ']', 'F', '[', '-', 'F', ']', 'F'], ['F', '[', 'F', '+', 'F', '+', 'F', ']', 'F', '[', 'F', '-', 'F', '-', 'F', ']'], ['F', 'F', '[', '+', 'F', ']', '[', '-', 'F', ']', 'F', 'F', '[', '-', 'F', '+', 'F', '+', 'F', ']'], ['F', '-', 'F', '+', 'F', '-', 'F', '-', 'F']];
 let selected_value;
 let button;
 let list;
 let rule = '';
 let ul;
 
-$(document).ready(function(){
+$(document).ready(function () {
 
   ul = document.getElementById('sortable');
 
-  $('#treeRadio').change(function(){
-      selected_value = $("input[name='tree']:checked").val();
-      while(ul.hasChildNodes()){
-        ul.removeChild(ul.firstChild)
-      }
-      switch(selected_value){
-        case '0':
-          for(let i of rules[0]){
-            let node = document.createElement("li");                 // Create a <li> node
-            let textnode = document.createTextNode(`${i}`);         // Create a text node
-            node.appendChild(textnode); 
-            ul.appendChild(node);
-          }
-          break;
-        case '1':
-          for(let i of rules[1]){
-            let node = document.createElement("li");                 // Create a <li> node
-            let textnode = document.createTextNode(`${i}`);         // Create a text node
-            node.appendChild(textnode); 
-            ul.appendChild(node);
-          }
-          break;
-        case '2':
-          for(let i of rules[2]){
-            let node = document.createElement("li");                 // Create a <li> node
-            let textnode = document.createTextNode(`${i}`);         // Create a text node
-            node.appendChild(textnode); 
-            ul.appendChild(node);
-          }
-          break;
-        case '3':
-          for(let i of rules[3]){
-            let node = document.createElement("li");                 // Create a <li> node
-            let textnode = document.createTextNode(`${i}`);         // Create a text node
-            node.appendChild(textnode); 
-            ul.appendChild(node);
-          }
-          break;
-        case '4':
-          for(let i of rules[4]){
-            let node = document.createElement("li");                
-            let textnode = document.createTextNode(`${i}`);         
-            node.appendChild(textnode); 
-            ul.appendChild(node);
-          }
-          break;
-        case '5':
-          for(let i of rules[5]){
-            let node = document.createElement("li");                 
-            let textnode = document.createTextNode(`${i}`);        
-            node.appendChild(textnode); 
-            ul.appendChild(node);
-          }
-          break;
-      }
+  $('#treeRadio').change(function () {
+    selected_value = $("input[name='tree']:checked").val();
+    while (ul.hasChildNodes()) {
+      ul.removeChild(ul.firstChild)
+    }
+    switch (selected_value) {
+      case '0':
+        for (let i of rules[0]) {
+          let node = document.createElement("li");                 // Create a <li> node
+          let textnode = document.createTextNode(`${i}`);         // Create a text node
+          node.appendChild(textnode);
+          ul.appendChild(node);
+        }
+        break;
+      case '1':
+        for (let i of rules[1]) {
+          let node = document.createElement("li");                 // Create a <li> node
+          let textnode = document.createTextNode(`${i}`);         // Create a text node
+          node.appendChild(textnode);
+          ul.appendChild(node);
+        }
+        break;
+      case '2':
+        for (let i of rules[2]) {
+          let node = document.createElement("li");                 // Create a <li> node
+          let textnode = document.createTextNode(`${i}`);         // Create a text node
+          node.appendChild(textnode);
+          ul.appendChild(node);
+        }
+        break;
+      case '3':
+        for (let i of rules[3]) {
+          let node = document.createElement("li");                 // Create a <li> node
+          let textnode = document.createTextNode(`${i}`);         // Create a text node
+          node.appendChild(textnode);
+          ul.appendChild(node);
+        }
+        break;
+      case '4':
+        for (let i of rules[4]) {
+          let node = document.createElement("li");
+          let textnode = document.createTextNode(`${i}`);
+          node.appendChild(textnode);
+          ul.appendChild(node);
+        }
+        break;
+      case '5':
+        for (let i of rules[5]) {
+          let node = document.createElement("li");
+          let textnode = document.createTextNode(`${i}`);
+          node.appendChild(textnode);
+          ul.appendChild(node);
+        }
+        break;
+    }
   });
 });
 
@@ -79,7 +79,7 @@ function setup() {
   gui = createGui();
 
   thetaSlider = createSliderV("theta", 30, 50, 32, 300, 0, TWO_PI);
-  thetaSlider.val=radians(25);
+  thetaSlider.val = radians(25);
 
   // radio = createRadio();
   // radio.option('A',0);
@@ -94,7 +94,7 @@ function setup() {
   // lenSlider.val=100;
   tree = new Lsystem();
   frameCount = 0;
-     
+
 }
 
 function draw() {
@@ -102,18 +102,18 @@ function draw() {
   drawGui();
 
   // radio.addEventListener("change", console.log(radio.selected()));
-    // if(radio.selected() == 'A'){
-    //   console.log('dicks');
-    // }
-  
-  
+  // if(radio.selected() == 'A'){
+  //   console.log('dicks');
+  // }
+
+
 
   list = selectAll('li');
-  rule='';
-  list.forEach(el=>rule+=el.html());
+  rule = '';
+  list.forEach(el => rule += el.html());
   tree.setRules(rule);
-  
-  if(thetaSlider.isChanged) {
+
+  if (thetaSlider.isChanged) {
     tree.setAngle(thetaSlider.val);
   }
   // if(lenSlider.isChanged){
@@ -121,11 +121,11 @@ function draw() {
   // }
 
   frameCount++;
-  if(frameCount%7===0){
-     tree.generate();
+  if (frameCount % 7 === 0) {
+    tree.generate();
   }
- 
-   
+
+
   tree.turtle();
 }
 
